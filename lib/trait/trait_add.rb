70a404d5b6b93d3f14602675274b0c9ed1d1ed82
Alan Vaudagna <alanvaudagna@gmail.com>
2018-05-11 23:30:45 -0300

    TraitAdding working with non conclicting.

--- lib/trait/trait_add.rb
+++ lib/trait/trait_add.rb
@@ -2,24 +2,22 @@ module TraitAdd
 
   def +(other)
     final_trait = Trait.new
-    final_trait.merge other.conflicts_chain
-    final_trait.merge conflicts_chain
-    final_trait.add_all_methods_non_conflicting(self, other)
+    final_trait.merge_conflict_chain other.conflicts_chain
+    final_trait.merge_conflict_chain conflicts_chain
+    final_trait.add_all_methods_non_conflicting(self)
+    final_trait.add_all_methods_non_conflicting(other)
+    final_trait
   end
 
-
-  def add_all_methods_non_conflicting(some_trait, other_trait)
-    add_methods_non_conflicting_left_join(some_trait, other_trait)
-    add_methods_non_conflicting_left_join(other_trait, some_trait)
+  def add_all_methods_non_conflicting(other_trait)
+    add_methods_non_conflicting_left_join(other_trait)
   end
 
-  private
-
-  def add_methods_non_conflicting_left_join(some_trait, other_trait)
-    method_some_trait_only = some_trait.methods - other_trait.methods
-    method_some_trait_only.each do |method_name|
+  def add_methods_non_conflicting_left_join(other_trait)
+    other_trait_methods_only = other_trait.methods - self.methods
+    other_trait_methods_only.each do |method_name|
       send(:define_singleton_method, method_name,
-           some_trait.singleton_method(method_name).to_proc)
+           other_trait.singleton_method(method_name).to_proc)
     end
   end
 end
