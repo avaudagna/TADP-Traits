4b54d40f98349273804b30d1138a351d4f764d80
Alan Vaudagna <alanvaudagna@gmail.com>
2018-05-12 01:24:55 -0300

    TraitAdding works for the 5 use cases.

--- lib/main.rb
+++ lib/main.rb
@@ -28,11 +28,36 @@ def init()
       un_numero * 0 + 50
     end
   end
+  Trait.define do
+    name :TraitB2
+    method :metodo3 do |un_numero|
+      un_numero * 0 + 33
+    end
+  end
+  Trait.define do
+    name :TraitB3
+    method :metodo3 do |un_numero|
+      un_numero * 0 + 33
+    end
+    method :metodo5 do |un_numero|
+      un_numero * 0 + 55
+    end
+  end
+  Trait.define do
+    name :TraitD
+    method :metodo4 do |un_numero|
+      un_numero * 0 + 40
+    end
+  end
+
 end
 
 def trait_adding()
   non_conflict_sum(TraitA, TraitB)
   simple_conflict_sum(TraitA, TraitA2)
+  one_brings_conflitcs_other_not(TraitD, simple_conflict_sum(TraitA, TraitA2))
+  both_brings_conflicts(simple_conflict_sum(TraitA, TraitA2), simple_conflict_sum(TraitB, TraitB))
+  both_brings_conflicts_and_non(simple_conflict_sum(TraitA, TraitA2), simple_conflict_sum(TraitB, TraitB2))
 end
 
 def simple_conflict_sum(one_trait, other_trait)
